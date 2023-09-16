12a13faf1f50062fd766eaad0348e40bd88133df
gtopinio <mctopinio@up.edu.ph>
2023-09-16 20:33:28 +0800

    update: initialize webcam component

---
+++ src/app/webcam/webcam.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-webcam',
+  templateUrl: './webcam.component.html',
+  styles: [
+  ]
+})
+export class WebcamComponent {
+
+}
